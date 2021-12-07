97224513b832bc33a1fb387a509b79f92f102d06
Caio Severino <kaiosev9@gmail.com>
2021-12-07 17:45:37 -0300

    script na pergunta feito

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,9 @@
-console.log('test');
+
+document.querySelectorAll('.accordion_pergunta').forEach((item) => {
+    item.addEventListener('click',(event) => {
+        console.log('test');
+        item.classList.toggle('show');
+        item.nextElementSibling.classList.toggle('open');
+    });
+    
+});
