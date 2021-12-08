9632e9d4781c7d1e9b0a5c25097edfda965ebbf3
Caio Severino <kaiosev9@gmail.com>
2021-12-08 11:53:27 -0300

    finalizado

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,8 +1,52 @@
 document.querySelectorAll('.accordion_pergunta').forEach((item) => {
     item.addEventListener('click',(event) => {
         console.log('test');
+        
+        let acc= item.nextElementSibling;
+
         item.classList.toggle('show');
-        item.nextElementSibling.classList.toggle('show');
+
+
+        if(!acc.classList.contains('show')){
+            console.log('open accordion');
+
+
+            acc.style.display = 'block';
+            let accHeight = acc.clientHeight;
+            console.log(accHeight);
+
+            setTimeout(() => {
+                acc.style.height = accHeight + 'px';
+                acc.style.display = '';
+                
+            },1);
+           ;
+          
+            acc.classList = 'accordion_collapse collapsing';
+            
+            setTimeout(() => {
+                console.log('open accordion content');
+                acc.classList = 'accordion_collapse show';
+                // acc.style.height = '';
+            }, 400);
+
+
+
+        }else{
+
+            acc.classList = 'accordion_collapse collapsing';
+          
+                    setTimeout(() =>{
+                        acc.style.height= '0px';
+                    },1);
+
+            setTimeout(() => {
+               acc.classList = 'accordion_collapse hidden'
+                console.log('close accordion content');
+                acc.style.height = '';
+            }, 400);
+        }   
     });
     
 });
+//item.nextElementSibling.classList.toggle('show');
